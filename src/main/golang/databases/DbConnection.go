2112d65e23227f8bafb5b2112b4667d47f47ee98
Mystic <215104920@qq.com>
2021-12-09 19:35:11 +0800

    style(fmt): reformatting the code

--- src/main/golang/databases/DbConnection.go
+++ src/main/golang/databases/DbConnection.go
@@ -1,39 +1,38 @@
 package databases
 
 import (
-    "log"
-    "os"
-
-    "github.com/zeimedee/go-postgres/models"
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
-    "gorm.io/gorm/logger"
+	"github.com/zeimedee/go-postgres/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
 )
 
 type Dbinstance struct {
-    Db *gorm.DB
+	Db *gorm.DB
 }
 
 var DB Dbinstance
 
-// ConnectDb
 func ConnectDb() {
-    dsn := "host=localhost user=postgres password='' dbname=go-db port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-        Logger: logger.Default.LogMode(logger.Info),
-    })
+	dsn := "host=localhost user=postgres password='' dbname=go-db port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 
-    if err != nil {
-        log.Fatal("Failed to connect to database. \n", err)
-        os.Exit(2)
-    }
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+	}
 
-    log.Println("connected")
-    db.Logger = logger.Default.LogMode(logger.Info)
-    log.Println("running migrations")
-    db.AutoMigrate(&models.Book{})
+	log.Println("connected")
+	db.Logger = logger.Default.LogMode(logger.Info)
+	log.Println("running migrations")
+	err = db.AutoMigrate(&models.Book{})
+	if err != nil {
+		return
+	}
 
-    DB = Dbinstance{
-        Db: db,
-    }
+	DB = Dbinstance{
+		Db: db,
+	}
 }
